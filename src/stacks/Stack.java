97fce00da1d28cf36735fbdb1503ec3aeee980e3
Tanbir Sohail <tanbirsohail@users.noreply.github.com>
2018-10-31 00:40:19 +0530

    Added display method, formatted code.

--- src/stacks/Stack.java
+++ src/stacks/Stack.java
@@ -11,32 +11,41 @@ public class Stack<T> {
 			this.data = data;
 		}
 	}
-	
+
 	private Node top;
-	
+
 	public boolean isNull() {
 		return top == null;
 	}
-	
+
 	public T peek() {
-		try {
+		if (top.data != null) {
 			return (T) top.data;
-		} catch (Exception e) {
-			System.out.println("Head is null, Queue is empty!");
+		} else {
+			System.out.println("Head is null, Stack is empty!");
 			return null;
 		}
 
 	}
-	
+
 	public void push(T data) {
 		Node<T> node = new Node<>(data);
 		node.next = top;
 		top = node;
 	}
-	
-	public T pop()	{
+
+	public T pop() {
 		T data = (T) top.data;
 		top = top.next;
 		return data;
+	}
+
+	void displayStack() {
+		Node<T> iterator = top;
+		while (iterator != null) {
+			System.out.print(iterator.data + " -> ");
+			iterator = iterator.next;
+		}
+		System.out.println();
 	}
 }
