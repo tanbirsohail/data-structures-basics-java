dace066edeca1aa62e71006786e466108e972a15
Tanbir Sohail <tanbirsohail@users.noreply.github.com>
2018-11-14 02:20:07 +0530

    Added extra utility functions

--- src/linked_lists/DemoClient.java
+++ src/linked_lists/DemoClient.java
@@ -17,10 +17,29 @@ public class DemoClient {
 		list.append(99);
 		list.printList();
 		
-		System.out.println("Appending 99 to the start of the list");
-		list.prepend(99);
+		System.out.println("Appending 0 to the start of the list");
+		list.prepend(0);
 		list.printList();
 		
+		System.out.println("The node at the end is having value: "+list.findLastNode().data);
+		System.out.println("The length of the list is :"+list.findLength());
+		
+		list.append(47);
+		list.append(22);
+		list.append(21);
+		
+		// Demo circular list property
+		System.out.println("Checking if list is circular: "+list.isListCircular());
+		LinkedListNode<Integer> tailNode = list.findLastNode();
+		
+		System.out.println("Latching the tail node to the fourth node in the list");
+		tailNode.next = list.getHead().next.next.next;
+		
+		System.out.println("Checking again if list is circular: "+list.isListCircular());
+		System.out.println("Printing the circular start point Node's value : "+list.findCircularStartNode().data);
+		
+		// Since the list is circular, printing will run into infinite loop
+		//list.printList();
 	}
 
 }
--- src/linked_lists/LinkedList.java
+++ src/linked_lists/LinkedList.java
@@ -6,15 +6,19 @@ package linked_lists;
 public class LinkedList<T> {
 
 	private LinkedListNode<T> head;
+
 	/**
 	 * Gets the head element of the linked list.
+	 * 
 	 * @return The head node
 	 */
 	public LinkedListNode<T> getHead() {
 		return head;
 	}
+
 	/**
 	 * Adds an element to the end of the list.
+	 * 
 	 * @param data The value of the element to be added.
 	 */
 	public void append(T data) {
@@ -28,8 +32,10 @@ public class LinkedList<T> {
 		}
 		current.next = new LinkedListNode<T>(data);
 	}
+
 	/**
 	 * Adds an element to the beginning of the list.
+	 * 
 	 * @param data The value for the node to be added.
 	 */
 	public void prepend(T data) {
@@ -37,6 +43,7 @@ public class LinkedList<T> {
 		newHead.next = head;
 		head = newHead;
 	}
+
 	/**
 	 * Displays the contents of the list.
 	 */
@@ -47,6 +54,66 @@ public class LinkedList<T> {
 			iterator = iterator.next;
 		}
 		System.out.println();
+	}
+
+	public LinkedListNode<T> findLastNode() {
+		LinkedListNode<T> current = this.head;
+		if (current == null) {
+			return null;
+		}
+		while (current.next != null) {
+			current = current.next;
+		}
+
+		return current;
+	}
+
+	public LinkedListNode<T> findCircularStartNode() {
+
+		LinkedListNode<T> slowRunner = this.head;
+		LinkedListNode<T> fastRunner = this.head;
+
+		// finding the collision point, will give the circular point in K iterations
+		while (fastRunner != null && fastRunner.next != null) {
+			fastRunner = fastRunner.next.next;
+			slowRunner = slowRunner.next;
+
+			if (fastRunner == slowRunner) {
+				break;
+			}
+		}
+		// now we are at collision point check if list has
+		// reached endpoint and hence is linear, no loop
+		if (fastRunner == null || fastRunner.next == null) {
+			return null;
+		}
+
+		// setting to head to meet at collision point
+		slowRunner = this.head;
+		while (slowRunner != fastRunner) {
+			slowRunner = slowRunner.next;
+			fastRunner = fastRunner.next;
+		}
+		return fastRunner;
+	}
+	
+	public boolean isListCircular() {
+		LinkedListNode<T> current = this.findCircularStartNode();
+		
+		return current != null;
+	}
+
+	public int findLength() {
+		int length = 0;
+		if (this.head == null) {
+			return length;
+		}
+		LinkedListNode<T> current = this.head;
+		while (current != null) {
+			length++;
+			current = current.next;
+		}
+		return length;
 	}
 
 }
