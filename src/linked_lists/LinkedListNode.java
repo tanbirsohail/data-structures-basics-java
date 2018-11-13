6a33feba6f50a38f7fd8ce0e55f53958b2d10cba
Tanbir Sohail <tanbirsohail@users.noreply.github.com>
2018-11-14 01:26:05 +0530

    Initial commit, external class for linked list node

---
+++ src/linked_lists/LinkedListNode.java
@@ -0,0 +1,12 @@
+package linked_lists;
+
+public class LinkedListNode<T> {
+	
+	protected T data;
+	protected LinkedListNode<T> next;
+	
+	protected LinkedListNode(T data) {
+		this.data = data;
+	}
+
+}
